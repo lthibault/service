cb43d062d10a1d977e9555509c9446467498508c
Louis Thibault <l.thibault@sentimens.com>
2020-03-23 17:11:47 +0000

    Rename Parallel to Group

--- parallel.go
+++ parallel.go
@@ -2,25 +2,33 @@ package service
 
 import "golang.org/x/sync/errgroup"
 
-// Parallel multiservice starts/stops each service in a separate goroutine, without
-// synchronization.  It is useful for large numbers of independent services, such as
-// those constituting a worker pool.
-type Parallel []Service
+// Group services into a multiservice that starts/stops each member concurrently, and
+// without synchronization.
+//
+// It is useful for large numbers of independent services, such as those constituting a
+// worker pool.
+type Group []Service
 
 // Start each service in its own goroutine.  There is no synchronization.
-func (p Parallel) Start() error {
-	var g errgroup.Group
-	for _, service := range p {
-		g.Go(service.Start)
+func (g Group) Start() error {
+	var xg errgroup.Group
+	for _, service := range g {
+		xg.Go(service.Start)
 	}
-	return g.Wait()
+	return xg.Wait()
 }
 
 // Stop each service in its own goroutine.  There is no synchronization.
-func (p Parallel) Stop() error {
-	var g errgroup.Group
-	for _, service := range p {
-		g.Go(service.Stop)
+func (g Group) Stop() error {
+	var xg errgroup.Group
+	for _, service := range g {
+		xg.Go(service.Stop)
 	}
-	return g.Wait()
+	return xg.Wait()
+}
+
+// Add a service
+func (g *Group) Add(s Service) *Group {
+	*g = append(*g, s)
+	return g
 }
