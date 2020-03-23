4d6eeecb06a7c9eaacfa02fc1efe96c44ffc7080
Louis Thibault <l.thibault@sentimens.com>
2020-03-23 23:36:18 +0000

    Test hierarchical MultiService

--- pkg/multiservice_test.go
+++ pkg/multiservice_test.go
@@ -42,3 +42,50 @@ func TestMultiService(t *testing.T) {
 	// N.B.:  we check that deferred-ordering is enforced
 	assert.Equal(t, []int{1, 2, -2, -1}, res)
 }
+
+func TestHierarchicalMultiService(t *testing.T) {
+	res := []int{}
+
+	svc := new(service.MultiService).
+		Append(service.MultiService{
+			service.Hook{
+				OnStart: func() error {
+					res = append(res, 1)
+					return nil
+				},
+				OnStop: func() error {
+					res = append(res, -1)
+					return nil
+				},
+			},
+			service.Hook{
+				OnStart: func() error {
+					res = append(res, 2)
+					return nil
+				},
+				OnStop: func() error {
+					res = append(res, -2)
+					return nil
+				},
+			},
+		}).
+		Append(service.Hook{
+			OnStart: func() error {
+				res = append(res, 3)
+				return nil
+			},
+			OnStop: func() error {
+				res = append(res, -3)
+				return nil
+			},
+		})
+
+	require.NoError(t, svc.Start())
+
+	assert.Equal(t, []int{1, 2, 3}, res)
+
+	require.NoError(t, svc.Stop())
+
+	// N.B.:  we check that deferred-ordering is enforced
+	assert.Equal(t, []int{1, 2, 3, -3, -2, -1}, res)
+}
