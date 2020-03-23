ddb84d595eb8119d215fd7e80f5b1836ec1fc85c
Louis Thibault <l.thibault@sentimens.com>
2020-03-23 14:39:13 +0000

    Remove contexts, add unit tests & fix bugs

--- service.go
+++ service.go
@@ -1,34 +1,32 @@
 package service
 
-import "context"
-
 // Service .
 type Service interface {
-	Start(context.Context) error
-	Stop(context.Context) error
+	Start() error
+	Stop() error
 }
 
 // Hook encapsulates startup and shutdown functions.
 type Hook struct {
-	OnStart, OnStop func(context.Context) error
+	OnStart, OnStop func() error
 }
 
 // Start call OnStart if it is defined.
-func (h Hook) Start(ctx context.Context) error {
+func (h Hook) Start() error {
 	if h.OnStart == nil {
 		return nil
 	}
 
-	return h.OnStart(ctx)
+	return h.OnStart()
 }
 
 // Stop calls OnStop if it is defined.
-func (h Hook) Stop(ctx context.Context) error {
+func (h Hook) Stop() error {
 	if h.OnStop == nil {
 		return nil
 	}
 
-	return h.OnStop(ctx)
+	return h.OnStop()
 }
 
 // MultiService is a collection of hooks that are run in-order on startup, and in
@@ -36,28 +34,28 @@ func (h Hook) Stop(ctx context.Context) error {
 type MultiService []Service
 
 // Append a hook to the MultiService
-func (ms *MultiService) Append(s Service) MultiService {
+func (ms *MultiService) Append(s Service) *MultiService {
 	*ms = append(*ms, s)
-	return *ms
+	return ms
 }
 
 // Start the service by running each hook's OnStart method.
-func (ms MultiService) Start(ctx context.Context) error {
-	return ms.foreach(ctx, func(ctx context.Context, s Service) error {
-		return s.Start(ctx)
+func (ms MultiService) Start() error {
+	return ms.foreach(func(s Service) error {
+		return s.Start()
 	})
 }
 
 // Stop the service by running each hook's OnStop method.
-func (ms MultiService) Stop(ctx context.Context) error {
-	return ms.reverse().foreach(ctx, func(ctx context.Context, s Service) error {
-		return s.Stop(ctx)
+func (ms MultiService) Stop() error {
+	return ms.reverse().foreach(func(s Service) error {
+		return s.Stop()
 	})
 }
 
-func (ms MultiService) foreach(ctx context.Context, f func(context.Context, Service) error) (err error) {
+func (ms MultiService) foreach(f func(Service) error) (err error) {
 	for _, service := range ms {
-		if err = f(ctx, service); err != nil {
+		if err = f(service); err != nil {
 			break
 		}
 	}
---
+++ service_test.go
@@ -0,0 +1,44 @@
+package service_test
+
+import (
+	"testing"
+
+	"github.com/lthibault/service"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMultiService(t *testing.T) {
+	res := []int{}
+
+	svc := new(service.MultiService).
+		Append(service.Hook{
+			OnStart: func() error {
+				res = append(res, 1)
+				return nil
+			},
+			OnStop: func() error {
+				res = append(res, -1)
+				return nil
+			},
+		}).
+		Append(service.Hook{
+			OnStart: func() error {
+				res = append(res, 2)
+				return nil
+			},
+			OnStop: func() error {
+				res = append(res, -2)
+				return nil
+			},
+		})
+
+	require.NoError(t, svc.Start())
+
+	assert.Equal(t, []int{1, 2}, res)
+
+	require.NoError(t, svc.Stop())
+
+	// N.B.:  we check that deferred-ordering is enforced
+	assert.Equal(t, []int{1, 2, -2, -1}, res)
+}
