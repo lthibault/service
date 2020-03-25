b7bd3459340cef16c0a7e8634fa02511c1bd0a5b
Louis Thibault <l.thibault@sentimens.com>
2020-03-25 23:39:20 +0000

    Remove process stuff

--- pkg/service.go
+++ pkg/service.go
@@ -18,51 +18,6 @@ type Service interface {
 	Stop() error
 }
 
-type process struct {
-	start, stop func() error
-}
-
-func (p process) Append(ss ...Service) Array {
-	return append(Array{p}, Array(ss)...)
-}
-
-func (p process) Go(ss ...Service) Set {
-	return append(Set{p}, Set(ss)...)
-}
-
-func (p process) Start() error {
-	if p.start == nil {
-		return nil
-	}
-
-	return p.start()
-}
-
-func (p process) Stop() error {
-	if p.start == nil {
-		return nil
-	}
-
-	return p.stop()
-}
-
-// New service
-func New(f ProcFunc) Service {
-	var p Proc
-	return process{
-		start: func() error {
-			p = goprocess.Background().Go(func(p goprocess.Process) {
-				f(p)
-			})
-
-			return nil
-		},
-		stop: func() error {
-			return p.Close()
-		},
-	}
-}
-
 // With bundles individual services into a group.
 func With(ss ...Service) Array {
 	return Array(ss)
